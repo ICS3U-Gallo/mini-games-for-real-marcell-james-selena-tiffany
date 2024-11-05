fc08477c5e5dd5f224959ec1a6761ce08a2808b1
hselenal <selenaliao2008@gmail.com>
2024-11-05 11:02:31 -0500

    Update selena_game.py
    Drew clown character

--- selena_game.py
+++ selena_game.py
@@ -14,8 +14,8 @@ clock = pygame.time.Clock()
 # ---------------------------
 # Initialize global variables
 
-circle_x = 200
-circle_y = 200
+char_x = 200
+char_y = 200
 
 # ---------------------------
 
@@ -32,7 +32,18 @@ while running:
     # DRAWING
     screen.fill((255, 255, 255))  # always the first drawing command
 
-    pygame.draw.circle(screen, (0, 0, 255), (circle_x, circle_y), 30)
+    # Drawing the clown
+    pygame.draw.circle(screen, (255, 225, 200), (char_x, char_y), 45)
+    pygame.draw.circle(screen, (255, 0, 0), (char_x, char_y + 10), 10)
+    pygame.draw.circle(screen, (0, 0, 0), (char_x - 15, char_y - 10), 5)
+    pygame.draw.circle(screen, (0, 0, 0), (char_x + 15, char_y - 10), 5)
+    pygame.draw.circle(screen, (0, 0, 255), (char_x - 40, char_y - 10), 15)
+    pygame.draw.circle(screen, (0, 0, 255), (char_x - 30, char_y - 30), 15)
+    pygame.draw.circle(screen, (0, 0, 255), (char_x - 10, char_y - 40), 15)
+    pygame.draw.circle(screen, (0, 0, 255), (char_x + 10, char_y - 40), 15)
+    pygame.draw.circle(screen, (0, 0, 255), (char_x + 30, char_y - 30), 15)
+    pygame.draw.circle(screen, (0, 0, 255), (char_x + 40, char_y - 10), 15)
+    pygame.draw.line(screen, (0, 0, 0), (char_x - 15, char_y + 28), (char_x + 15, char_y + 28), 2)
 
     # Must be the last two lines
     # of the game loop
