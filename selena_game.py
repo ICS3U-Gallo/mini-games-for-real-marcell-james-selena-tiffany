93a2593fb72df83a3ec70f4f668666d123756e0f
hselenal <selenaliao2008@gmail.com>
2024-11-07 11:03:12 -0500

    Drew the tires continuously falling, drew the tire occasionally rolling on the ground

--- selena_game.py
+++ selena_game.py
@@ -14,19 +14,20 @@ clock = pygame.time.Clock()
 # ---------------------------
 # Initialize global variables
 
-char_x = WIDTH/2
+char_x = 0
 char_y = 400
 going_right = False 
 going_left = False 
 jumping = False 
-jump_speed = 20 
-curtain_x = 50 
+jump_speed = 12 
 tire_list = [] 
 for i in range(10): 
-    tire_x = random.randrange(WIDTH)
-    tire_y = random.randrange(HEIGHT) 
-    tire = (tire_x, -tire_y)
+    tire_x = random.randrange(100, WIDTH)
+    tire_y = random.randrange(-600, -30) 
+    tire = [tire_x, tire_y]
     tire_list.append(tire)
+
+roll_x = -100 
 
 # ---------------------------
 
@@ -61,6 +62,18 @@ while running:
         jump_speed *= -1
     if char_y > 400: 
         jumping = False 
+    
+    for item in tire_list: 
+        item[1] += 7 
+        if item[1] > HEIGHT + 50: 
+            item[0] = random.randrange(100, WIDTH)
+            item[1] = random.randrange(-100, -30)
+
+    chance = random.randrange(100)
+    if chance == 1 and roll_x < -100: 
+        roll_x = 700
+
+    roll_x -= 15 
 
     # DRAWING
     screen.fill((175, 25, 25))  # always the first drawing command
@@ -71,18 +84,28 @@ while running:
     pygame.draw.circle(screen, (175, 100, 50), (WIDTH/2, HEIGHT + 550), 700)
 
     # Drawing the clown
-    pygame.draw.circle(screen, (255, 225, 200), (char_x, char_y), 40)
-    pygame.draw.circle(screen, (255, 0, 0), (char_x, char_y + 7), 10)
+    pygame.draw.circle(screen, (255, 225, 200), (char_x, char_y), 35)
+    pygame.draw.circle(screen, (255, 0, 0), (char_x, char_y + 5), 10)
     pygame.draw.circle(screen, (0, 0, 0), (char_x - 15, char_y - 13), 5)
     pygame.draw.circle(screen, (0, 0, 0), (char_x + 15, char_y - 13), 5)
-    pygame.draw.circle(screen, (0, 0, 255), (char_x - 40, char_y - 10), 13)
-    pygame.draw.circle(screen, (0, 0, 255), (char_x - 30, char_y - 30), 13)
-    pygame.draw.circle(screen, (0, 0, 255), (char_x - 10, char_y - 40), 13)
-    pygame.draw.circle(screen, (0, 0, 255), (char_x + 10, char_y - 40), 13)
-    pygame.draw.circle(screen, (0, 0, 255), (char_x + 30, char_y - 30), 13)
-    pygame.draw.circle(screen, (0, 0, 255), (char_x + 40, char_y - 10), 13)
-    pygame.draw.line(screen, (0, 0, 0), (char_x - 15, char_y + 25), (char_x + 15, char_y + 25), 2)
+    pygame.draw.circle(screen, (0, 0, 255), (char_x - 38, char_y - 8), 13)
+    pygame.draw.circle(screen, (0, 0, 255), (char_x - 28, char_y - 28), 13)
+    pygame.draw.circle(screen, (0, 0, 255), (char_x - 10, char_y - 38), 13)
+    pygame.draw.circle(screen, (0, 0, 255), (char_x + 10, char_y - 38), 13)
+    pygame.draw.circle(screen, (0, 0, 255), (char_x + 28, char_y - 28), 13)
+    pygame.draw.circle(screen, (0, 0, 255), (char_x + 38, char_y - 8), 13)
+    pygame.draw.line(screen, (0, 0, 0), (char_x - 15, char_y + 23), (char_x + 15, char_y + 23), 2)
 
+    # Drawing the tires falling down 
+    for item in tire_list: 
+        pygame.draw.circle(screen, (0, 0, 0), (item[0], item[1]), 30)
+        pygame.draw.circle(screen, (128, 128, 128), (item[0], item[1]), 25)
+        pygame.draw.circle(screen, (0, 0, 0), (item[0], item[1]), 15)
+    
+    # Drawing the tire rolling on the ground 
+    pygame.draw.circle(screen, (0, 0, 0), (roll_x, 450), 25)
+    pygame.draw.circle(screen, (128, 128, 128), (roll_x, 450), 20)
+    pygame.draw.circle(screen, (0, 0, 0), (roll_x, 450), 10)
 
     # Must be the last two lines
     # of the game loop
